cb03ec90b48683f9aa319cb356e5ec0c33d71f62
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-07-29 23:44:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -13,11 +13,3 @@ end
  
   
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def display_board 
-  puts " #{board[0]} | #{board[1]} | #{board[2]}"
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]}"
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]}"
-end
