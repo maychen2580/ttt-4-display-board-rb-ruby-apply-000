d638937a66db2f5afdec91d0ab3517ca79fa6cad
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-07-30 00:01:21 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,4 +11,8 @@ def display_board (board)
 end 
  
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
+  puts " #{board[0]} | #{board[1]} | #{board[2]}"
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]}"
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]}"
